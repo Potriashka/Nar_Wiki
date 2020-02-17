7fa695b5f86d44d3492634a850fc071a744c9c21
Potriashka <54754814+Potriashka@users.noreply.github.com>
2020-02-17 16:56:16 +0200

    Update application.py

--- application.py
+++ application.py
@@ -6,6 +6,10 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/#')
+def reshetka():
+    return render_template('reshetka.html')
+
 @app.route('/anime')
 def anime():
     return render_template('anime.html')
