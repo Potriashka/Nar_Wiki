425cffbd97b704d7bc57df64bada5f91979aa824
Potriashka <54754814+Potriashka@users.noreply.github.com>
2020-02-18 00:17:16 +0200

    Update application.py

--- application.py
+++ application.py
@@ -50,5 +50,13 @@ def chunin():
 def boruto():
     return render_template('boruto.html')
 
+@app.route("/search", methods=[GET, POST])
+def search():
+	search = request.form["search"]
+	if search in website_names:
+	 	return render_template(f"{search}.html")
+	else:
+		return render_template("404.html") # сам напишешь
+
 if __name__ == "__main__":
     app.run(debug=True)
